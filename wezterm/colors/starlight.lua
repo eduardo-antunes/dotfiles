52c8bc7ca5ce23c75967aa15e1c090876b45bb56
Eduardo Antunes dos Santos Vieira <eduardoantunes986@gmail.com>
2025-11-16 13:56:51 -0300

    wezterm: adiciona tema starlight v4, simplifica

---
+++ wezterm/colors/starlight.lua
@@ -0,0 +1,33 @@
+-- Tema starlight v4, o padrão do foot e meu tema de terminal favorito
+-- Você pode encontrá-lo em: https://github.com/CosmicToast/starlight
+
+return {
+  background    = "#242424",
+  foreground    = "#FFFFFF",
+  cursor_bg     = "#E6E6E6",
+  cursor_fg     = "#242424",
+  cursor_border = "#E6E6E6",
+  selection_fg  = "#E6E6E6",
+  selection_bg  = "#839496",
+  ansi = {
+    "#242424", -- preto
+    "#F62B5A", -- vermelho
+    "#47B413", -- verde
+    "#E3C401", -- amarelo
+    "#24ACD4", -- azul
+    "#F2AFFD", -- magenta
+    "#13C299", -- ciano
+    "#E6E6E6", -- branco
+  },
+  -- Mesmo que acima, mas "brilhante"
+  brights = {
+    "#616161",
+    "#FF4D51",
+    "#35D450",
+    "#E9E836",
+    "#5DC5F8",
+    "#FEABF2",
+    "#24DFC4",
+    "#FFFFFF",
+  },
+}
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,83 +1,41 @@
-local wezterm = require "wezterm"
+-- Configuração do wezterm, um terminal multiplataforma rico em
+-- funcionalidades. Atualmente é a única aplicação cotidiana pra mim que eu
+-- utilizo no windows fora do WSL
+
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-local font_size = 10
+-- Algumas configurações precisam ser ajustadas no windows
+local on_windows = wezterm.target_triple:find("windows")
 
--- WSL -------------------------------------------------------------------------
+config.default_cwd = wezterm.home_dir
+if on_windows then config.default_domain = "WSL:Ubuntu-24.04" end
 
-if wezterm.target_triple:find "windows" then
-  config.default_domain = "WSL:Ubuntu-24.04"
-  config.default_cwd = wezterm.home_dir
-  -- In Windows, I guess because of rendering distances, the font size needs to
-  -- be bigger in order for the font to show the same way
-  font_size = 14
-end
-
--- Aparência e fontes ----------------------------------------------------------
+-- Aparência -------------------------------------------------------------------
 
 config.tab_max_width = 64
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
-config.window_padding = { left = 2, top = 0, bottom = 0, right = 2 }
+config.window_padding = { left = 2, right = 2, top = 0, bottom = 0 }
 
-config.font_size = font_size
+config.line_height = 1.05
 config.font = wezterm.font "Hack"
--- With the options below, font rendering looks wonderfully sharp in every OS
+config.font_size = on_windows and 14 or 10
+
 config.freetype_load_target = "Light"
 config.freetype_render_target = "HorizontalLcd"
 
+config.color_schemes = {
+  ["starlight"] = require("colors.starlight")
+}
+config.color_scheme = "starlight"
+
 -- Atalhos de teclado ----------------------------------------------------------
 
-config.leader = { key = "a", mods = "CTRL" }
 config.keys = {
   { key = "F11", action = wezterm.action.ToggleFullScreen },
-  { mods = "LEADER|CTRL", key = "a",
-    action = wezterm.action.SendKey { mods = "CTRL", key = "a" },
-  },
-  { mods = "LEADER", key = "c",
-    action = wezterm.action.SpawnTab "CurrentPaneDomain"
-  },
-  { mods = "LEADER", key = "w",
-    action = wezterm.action.CloseCurrentTab { confirm = true }
-  },
-  { mods = "LEADER", key = "q",
-    action = wezterm.action.CloseCurrentPane { confirm = true }
-  },
-  { mods = "LEADER", key = "v",
-    action = wezterm.action.SplitHorizontal {}
-  },
-  { mods = "LEADER", key = "h",
-    action = wezterm.action.SplitVertical {}
-  },
-  { mods = "LEADER", key = "1",
-    action = wezterm.action.ActivateTab(0)
-  },
-  { mods = "LEADER", key = "2",
-    action = wezterm.action.ActivateTab(1)
-  },
-  { mods = "LEADER", key = "3",
-    action = wezterm.action.ActivateTab(2)
-  },
-  { mods = "LEADER", key = "4",
-    action = wezterm.action.ActivateTab(3)
-  },
-  { mods = "LEADER", key = "5",
-    action = wezterm.action.ActivateTab(4)
-  },
-  { mods = "LEADER", key = "6",
-    action = wezterm.action.ActivateTab(5)
-  },
-  { mods = "LEADER", key = "7",
-    action = wezterm.action.ActivateTab(6)
-  },
-  { mods = "LEADER", key = "8",
-    action = wezterm.action.ActivateTab(7)
-  },
-  { mods = "LEADER", key = "9",
-    action = wezterm.action.ActivateTab(8)
-  },
 }
 
 return config
