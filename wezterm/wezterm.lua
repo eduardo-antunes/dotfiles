98e1f45de36dabe7873e203c9281c39c37ca8776
Eduardo Antunes dos Santos Vieira <eduardoantunes986@gmail.com>
2025-11-30 11:05:24 -0300

    wezterm: disable ligatures
    For consistency with foot

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -19,6 +19,7 @@ config.window_padding = { left = 2, right = 2, top = 0, bottom = 0 }
 config.font = wezterm.font "Iosevka"
 config.font_size = on_windows and 14 or 11
 
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 if on_windows then config.freetype_load_target = "Light" end
 config.freetype_render_target = "HorizontalLcd"
 
