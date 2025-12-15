4ac619156a81e591aaddd08f112805b2adc60092
Eduardo Antunes dos Santos Vieira <eduardoantunes986@gmail.com>
2025-12-15 20:44:44 -0300

    wezterm: reintroduce on_windows, font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+local on_windows = wezterm.target_triple:find "windows"
+
 config.default_cwd = wezterm.home_dir
 if on_windows then config.default_domain = "WSL:Ubuntu-24.04" end
 
@@ -14,7 +16,7 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = { left = 2, right = 2, top = 0, bottom = 0 }
 
 config.font = wezterm.font "Iosevka"
-config.font_size = 11
+config.font_size = on_windows and 12 or 11
 
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.freetype_render_target = "HorizontalLcd"
